dd1ff5e7367765b36f89359c31c5212f33cf7494
Yumozh <juliiamozhchil@gmail.com>
2026-04-14 11:24:45 -0500

    Request input from users about cellphone

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,7 +1,26 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
     public static void main(String[] args) {
 
+        CellPhone phone = new CellPhone();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the serial number?");
+        phone.setSerialNumber(input.nextInt());
+
+        System.out.println("What model is the phone?");
+        phone.setModel(input.nextLine());
+
+        System.out.println("Who is the carrier?");
+        phone.setCarrier(input.nextLine());
+
+        System.out.println("What is the phone number?");
+        phone.setPhoneNumber(input.nextLine());
+
+        System.out.println("Who is the owner of the phone?");
+        phone.setOwner(input.nextLine());
     }
 }
