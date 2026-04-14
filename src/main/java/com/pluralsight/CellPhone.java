fbde6c9b06a919b3272c15c710e56107f73d40fb
Yumozh <juliiamozhchil@gmail.com>
2026-04-14 11:02:04 -0500

    Initiate commit and create a CellPhone class

---
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -0,0 +1,57 @@
+package com.pluralsight;
+
+public class CellPhone {
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    public CellPhone() {
+        this.serialNumber = 0;
+        this.model = " ";
+        this.carrier = " ";
+        this.phoneNumber = " ";
+        this.owner = " ";
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
